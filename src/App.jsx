8bfe9f927c1314f90017fc0751f7695a704e2f69
Chandra Lama <iamme24cl@gmail.com>
2024-06-17 18:09:12 -0700

    added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import Navbar from "./components/navbar/Navbar";
+import "./app.scss"
+
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar /> 
+      </section>
+      <section id="Services">
+        Parallax - services
+      </section>
+      <section>
+        Services
+      </section>
+      <section id="Portfolio">
+        Parallax - portfolio
+      </section>
+      <section id="Contact">
+        Contact
+      </section>
+    </div>
+  );
 };
 
 export default App;
